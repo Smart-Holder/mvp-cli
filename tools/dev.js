57520096508bd3976d57a9e35f33b9114215d6ba
louistru <louistru@hotmail.com>
2022-01-13 14:11:42 +0800

    update webpkit

--- tools/dev.js
+++ tools/dev.js
@@ -1,14 +1,17 @@
 
-// const path = require('path');
 
-const webpack = require('webpack');
+const cfg = require('webpkit/tools/cfg');
+
+cfg.env = {
+	NODE_ENV: `"${cfg.NODE_ENV}"`,
+	WALLET: !!process.env.WALLET,
+};
+
 const tools = require('webpkit/tools');
 
 tools.module.rules[0].exclude = /((typeof|_bigint)\.js)|node_modules/;
 tools.module.rules[0].use = { loader: 'ts-loader' };
 // webpack.module.rules[1].exclude = /mvp-ser/;
 // webpack.module.rules[1].include.push(path.resolve('./deps/mvp-ser/src/models'));
-
-tools.plugins.push(new webpack.DefinePlugin({ 'process.env.WALLET': `"${process.env.WALLET}"` }));
 
 module.exports = tools;
