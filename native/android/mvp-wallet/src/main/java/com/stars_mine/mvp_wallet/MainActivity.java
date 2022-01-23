6dda01d6c81c21aa724e972402c7f6b760472efe
louistru <louistru@hotmail.com>
2022-01-23 17:12:07 +0800

    update back button event

--- native/android/mvp-wallet/src/main/java/com/stars_mine/mvp_wallet/MainActivity.java
+++ native/android/mvp-wallet/src/main/java/com/stars_mine/mvp_wallet/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -229,6 +230,13 @@ public class MainActivity extends AppCompatActivity
 			 }
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			_webview.goBack();
+		}
+		return true;
 	}
 
 }
