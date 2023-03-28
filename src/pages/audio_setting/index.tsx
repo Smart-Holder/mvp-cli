c6f0cc8a6dba836385e4646dc4dc69c033a1b940
李学研 <lixueyan@stars-mine.com>
2023-03-28 11:11:37 +0800

    修改显示svg

--- src/pages/audio_setting/index.tsx
+++ src/pages/audio_setting/index.tsx
@@ -1,39 +1,39 @@
-import { withTranslation } from 'react-i18next';
-import { React } from 'webpkit/mobile';
-import NavPage from '../../nav';
-import Header from '../../util/header';
-import { getScreenSettings, screenVolume } from '../../models/device';
-import Loading from '../../../deps/webpkit/lib/loading';
-import { Slider } from 'antd';
+import { withTranslation } from "react-i18next";
+import { React } from "webpkit/mobile";
+import NavPage from "../../nav";
+import Header from "../../util/header";
+import { getScreenSettings, screenVolume } from "../../models/device";
+import Loading from "../../../deps/webpkit/lib/loading";
+import { Slider } from "antd";
 
-import './index.scss';
+import "./index.scss";
 
-class AudioSetting extends NavPage<{ address: string, env: string }> {
-
+class AudioSetting extends NavPage<{ address: string; env: string }> {
 	state = {
 		volume: 0,
-		dsq_id: 0
-	}
+		dsq_id: 0,
+	};
 
 	async triggerLoad() {
 		let { address } = this.params;
-		let l = await Loading.show(this.t('正在加载屏幕设置'));
+		let l = await Loading.show(this.t("正在加载屏幕设置"));
 		// 获取设备当前设置参数
-		getScreenSettings(address).then(({ volume, env }) => {
-			let unit = env.includes('t982') ? 20 : 3;
-			volume = volume / unit;
+		getScreenSettings(address)
+			.then(({ volume, env }) => {
+				let unit = env.includes("t982") ? 20 : 3;
+				volume = volume / unit;
 
-			this.setState({ volume });
-		}).catch((err: any) => {
-			alert(err.message);
-		}).finally(() => l.close());
+				this.setState({ volume });
+			})
+			.catch((err: any) => {
+				alert(err.message);
+			})
+			.finally(() => l.close());
 	}
 
 	sliderChange(e: number) {
-
-
 		let { address, env } = this.params;
-		let unit = env.includes('t982') ? 20 : 3;
+		let unit = env.includes("t982") ? 20 : 3;
 		let volume = e * unit;
 		let { dsq_id } = this.state;
 		// let dsq_id = 0;
@@ -42,33 +42,31 @@ class AudioSetting extends NavPage<{ address: string, env: string }> {
 			screenVolume(address, { volume, volumeScale: ((100 / 5) * e) / 100 });
 		}, 500);
 		this.setState({ volume: e, dsq_id: newDsqId });
-
 	}
 
 	render() {
 		let { t } = this;
 		let { volume } = this.state;
-		return <div className="audio_page">
-			<Header title={t("音量")} page={this} />
+		return (
+			<div className="audio_page">
+				<Header title={t("音量")} page={this} />
 
-			<div className="setting_box">
+				<div className="setting_box">
+					<div className="setting_title">{t("调整音量")}</div>
 
-				<div className="setting_title">{t("调整音量")}</div>
-
-				<div className="card_box">
-
-
-					<div className="bottom_part">
-						<img className="voice_low_img" src={require('../../assets/voice_low.png')} />
-						<div className="slider_box">
-							<Slider trackStyle={{ background: 'linear-gradient(-89deg, #3076F5, #5DB7F7)', height: '.1rem' }} handleStyle={{ backgroundColor: '#fff', border: 0, boxShadow: '0px 5px 32px 0px #131212' }} max={5} min={0} value={volume} dots step={1} onChange={this.sliderChange.bind(this)} />
+					<div className="card_box">
+						<div className="bottom_part">
+							<img className="voice_low_img" src={require("../../assets/voice_low.png")} />
+							<div className="slider_box">
+								<Slider trackStyle={{ background: "linear-gradient(-89deg, #3076F5, #5DB7F7)", height: ".1rem" }} handleStyle={{ backgroundColor: "#fff", border: 0, boxShadow: "0px 5px 32px 0px #131212" }} max={5} min={0} value={volume} dots step={1} onChange={this.sliderChange.bind(this)} />
+							</div>
+							<img className="voice_height_img" src={require("../../assets/voice_high.png")} />
 						</div>
-						<img className="voice_height_img" src={require('../../assets/voice_high.png')} />
 					</div>
 				</div>
 			</div>
-		</div>
+		);
 	}
 }
 
-export default withTranslation('translations', { withRef: true })(AudioSetting);
+export default withTranslation("translations", { withRef: true })(AudioSetting);
--- src/pages/preview_nft_list/index.tsx
+++ src/pages/preview_nft_list/index.tsx
@@ -1,15 +1,15 @@
 import { NFT } from "../../models";
-import { React } from 'webpkit/mobile';
-import { Tabs } from 'antd-mobile';
-import { Empty } from 'antd';
-import * as device from '../../models/device';
-import Header from '../../util/header';
+import { React } from "webpkit/mobile";
+import { Tabs } from "antd-mobile";
+import { Empty } from "antd";
+import * as device from "../../models/device";
+import Header from "../../util/header";
 
-import './index.scss';
+import "./index.scss";
 import NavPage from "../../nav";
-import { LoadingOutlined } from '@ant-design/icons';
+import { LoadingOutlined } from "@ant-design/icons";
 import chain from "../../chain";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 import { getNFTByOwnerPage, getNFTByOwner } from "../../models/nft";
 import { withTranslation } from "react-i18next";
 import ImageMogr from "../../components/image_mogr";
@@ -17,9 +17,9 @@ import Loading from "../../../deps/webpkit/lib/loading";
 import { getScreenSettings } from "../../models/device";
 
 class PreviewNftCard extends NavPage<{ address: string }> {
-
 	tabsConfig = [
-		{ title: this.t('我的NFT'), index: 0 }, { title: this.t('设备NFT'), index: 1 }
+		{ title: this.t("我的NFT"), index: 0 },
+		{ title: this.t("设备NFT"), index: 1 },
 	];
 	state = {
 		tabs: this.tabsConfig,
@@ -32,23 +32,25 @@ class PreviewNftCard extends NavPage<{ address: string }> {
 		curPage: 1,
 		nftList: [] as NFT[],
 		screenWidth: 1920,
-		screenHeight: 1080
-	}
+		screenHeight: 1080,
+	};
 
 	async triggerLoad() {
 		let { address } = this.params;
 
-		let l = await Loading.show(this.t('正在加载屏幕设置'));
+		let l = await Loading.show(this.t("正在加载屏幕设置"));
 
 		// 获取设备当前设置参数
-		getScreenSettings(address).then(({ screenHeight, screenWidth }) => {
-			this.setState({ screenHeight, screenWidth }, () => {
-				this.getNftList(undefined, 0);
-			});
-		}).catch((err: any) => {
-			alert(err.message);
-		}).finally(() => l.close());
-
+		getScreenSettings(address)
+			.then(({ screenHeight, screenWidth }) => {
+				this.setState({ screenHeight, screenWidth }, () => {
+					this.getNftList(undefined, 0);
+				});
+			})
+			.catch((err: any) => {
+				alert(err.message);
+			})
+			.finally(() => l.close());
 	}
 
 	// 获取nft列表
@@ -58,24 +60,29 @@ class PreviewNftCard extends NavPage<{ address: string }> {
 		let { screenHeight, screenWidth } = this.state;
 		// let { screenWidth, screenHeight } = this.props;
 
-
 		let owner = await chain.getDefaultAccount();
 		let ownerAddress = !mode ? owner : address;
 		let { curPage, nftList: preNftList } = this.state;
-		let nftList: NFT[] = list?.length ? list : await getNFTByOwnerPage({
-			owner: ownerAddress, screenWidth, screenHeight, curPage, pageSize: 999,
-			address: this.params.address
-		});
+		let nftList: NFT[] = list?.length
+			? list
+			: await getNFTByOwnerPage({
+					owner: ownerAddress,
+					screenWidth,
+					screenHeight,
+					curPage,
+					pageSize: 999,
+					address: this.params.address,
+			  });
 		let leftNftList: NFT[] = [];
 		let rightNftList: NFT[] = [];
 		let videoNftList: NFT[] = [];
 		let imgNftList: NFT[] = [];
 		let allNftList: NFT[] = [];
-		nftList.forEach(item => {
+		nftList.forEach((item) => {
 			let { image, imageOrigin, media, mediaOrigin } = item;
 			if (image && imageOrigin) {
 				// item.media.match(/\.mp4/i) ? videoNftList.push(item) : imgNftList.push(item)
-				(!media.match(/\.mp4/i) && !image.match(/\.gif/i)) && imgNftList.push(item)
+				!media.match(/\.mp4/i) && !image.match(/\.gif|svg/i) && imgNftList.push(item);
 
 				// imgNftList.push(item);
 			}
@@ -87,14 +94,12 @@ class PreviewNftCard extends NavPage<{ address: string }> {
 			!Boolean(index % 2) ? leftNftList.push(item) : rightNftList.push(item);
 		});
 
-
 		// let hasMore = Boolean((!mode ? imgNftList?.length : nftList?.length) >= 16);
 		let hasMore = Boolean(nftList?.length >= 999);
 
 		// console.log(nftList.length, 'nftList', nftList?.length >= 16, hasMore, 'hasMore');
 
 		let newState: any = { leftNftList, rightNftList, videoNftList, loading: false, nftList: allNftList, hasMore };
-
 
 		this.setState({ ...newState, mode });
 		return allNftList;
@@ -107,10 +112,11 @@ class PreviewNftCard extends NavPage<{ address: string }> {
 	}
 
 	rendNftItem(nft: NFT) {
-		return <div key={nft.id} onClick={this.nftItemClick.bind(this, nft)} className="nft_item">
-			{nft.media.match(/\.mp4/i) ? <video controls src={nft.media} poster={nft.image}></video> :
-				<ImageMogr className="nft_img" src={nft.image} nft={nft} /> || <img src={nft.image} alt="" />}
-		</div>
+		return (
+			<div key={nft.id} onClick={this.nftItemClick.bind(this, nft)} className="nft_item">
+				{nft.media.match(/\.mp4/i) ? <video controls src={nft.media} poster={nft.image}></video> : <ImageMogr className="nft_img" src={nft.image} nft={nft} /> || <img src={nft.image} alt="" />}
+			</div>
+		);
 	}
 
 	// 下拉加载
@@ -121,62 +127,60 @@ class PreviewNftCard extends NavPage<{ address: string }> {
 		});
 	}
 
-
 	render() {
 		let t = this.t;
 		let { tabs, leftNftList, rightNftList, loading, hasMore, nftList } = this.state;
-		let loader = <div className="bottom_box" > <LoadingOutlined className="loading_icon" /></div>;
+		let loader = (
+			<div className="bottom_box">
+				{" "}
+				<LoadingOutlined className="loading_icon" />
+			</div>
+		);
 
-		let endMessage = <div className="bottom_box">{t('已经是全部数据了')}</div>;
+		let endMessage = <div className="bottom_box">{t("已经是全部数据了")}</div>;
 
-		return <div className="setting_card_box pre_nftlist_page">
+		return (
+			<div className="setting_card_box pre_nftlist_page">
+				<Header title={t("预览设置")} page={this} />
 
-			<Header title={t("预览设置")} page={this} />
+				<div className="nft_list_page">
+					<div className="nft_list_box">
+						<Tabs
+							tabBarBackgroundColor={"transparent"}
+							tabBarUnderlineStyle={{ border: 0, height: "3px", background: "linear-gradient(90deg, #4881FA, #6ED6F5)", borderRadius: "3px" }}
+							animated={false}
+							onChange={(e) => {
+								this.setState({ curPage: 1, nftList: [], rightNftList: [], leftNftList: [] }, () => {
+									this.getNftList(undefined, e.index);
+								});
+							}}
+							tabBarActiveTextColor={"#1677ff"}
+							tabs={tabs}
+							initialPage={0}
+						>
+							<div className="item_page" id="preview_scroll_box">
+								<InfiniteScroll key={"preview_scroll_box"} dataLength={nftList.length} next={this.loadMoreData.bind(this)} hasMore={hasMore} loader={loader} endMessage={nftList.length ? endMessage : ""} scrollableTarget={"preview_scroll_box"} scrollThreshold={"300px"}>
+									{nftList.length ? (
+										<div className="nft_list">
+											<div className="left_box">{leftNftList.map((item) => this.rendNftItem(item))}</div>
 
-			<div className="nft_list_page">
-
-				<div className="nft_list_box">
-
-					<Tabs tabBarBackgroundColor={"transparent"} tabBarUnderlineStyle={{ border: 0, height: '3px', background: 'linear-gradient(90deg, #4881FA, #6ED6F5)', borderRadius: '3px' }} animated={false} onChange={(e) => {
-						this.setState({ curPage: 1, nftList: [], rightNftList: [], leftNftList: [] }, () => {
-							this.getNftList(undefined, e.index);
-						});
-					}} tabBarActiveTextColor={'#1677ff'} tabs={tabs} initialPage={0}>
-						<div className="item_page" id="preview_scroll_box">
-							<InfiniteScroll
-								key={"preview_scroll_box"}
-								dataLength={nftList.length}
-								next={this.loadMoreData.bind(this)}
-								hasMore={hasMore}
-								loader={loader}
-								endMessage={nftList.length ? endMessage : ''}
-								scrollableTarget={"preview_scroll_box"}
-								scrollThreshold={'300px'}
-							>
-								{nftList.length ? <div className="nft_list">
-									<div className="left_box">
-										{leftNftList.map(item => this.rendNftItem(item))}
-									</div>
-
-									<div className="right_box">
-										{rightNftList.map(item => this.rendNftItem(item))}
-									</div>
-								</div> : !loading && <Empty style={{ marginTop: '2rem', color: '#ccc' }} image={require('../../assets/empty_img.png')} description={t("暂无NFT，请添加NFT至设备")} />}
-							</InfiniteScroll>
-							{/* {loading && <div className="list_loading">
+											<div className="right_box">{rightNftList.map((item) => this.rendNftItem(item))}</div>
+										</div>
+									) : (
+										!loading && <Empty style={{ marginTop: "2rem", color: "#ccc" }} image={require("../../assets/empty_img.png")} description={t("暂无NFT，请添加NFT至设备")} />
+									)}
+								</InfiniteScroll>
+								{/* {loading && <div className="list_loading">
 						<LoadingOutlined />
 					</div>} */}
-						</div>
-					</Tabs>
+							</div>
+						</Tabs>
+					</div>
 				</div>
-
 			</div>
-
-		</div>;
+		);
 	}
-
 }
 
-
 // export default PreviewNftCard;
-export default withTranslation('translations', { withRef: true })(PreviewNftCard);
+export default withTranslation("translations", { withRef: true })(PreviewNftCard);
