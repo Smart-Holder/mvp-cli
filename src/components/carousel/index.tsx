2ee3330d905a03947745f1bf9c35e336478c2b63
fanchunbo <fanbobo1996@gmial.com>
2023-07-27 17:48:07 +0800

    :bento: feat: 修改扫码绑定tips文案

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -42,21 +42,34 @@ export const BindDeviceCarousel = (props: IbindDeviceCarouselProps) => {
 	console.log(locLanguage,"locLanguage");
 	let isZhCn = locLanguage === 'ZH-CN';
 
+	// let carouselSetupTypeConfig:any = {
+	// 	TokenPocket: [
+	// 		{ title: t('第一步：进入钱包主页，点击右上角扫一扫'), img: require(`../../assets/tp_setp_1${isZhCn ?'' :'_en'}.jpg`) },
+	// 		{ title: <div>{t('第二步：扫码绑定成功')}</div>, img: require(`../../assets/step_3${isZhCn ?'' :'_en'}.png`) },
+	// 	],
+	// 	MateMask: [
+	// 		// { title: <div>{t('第一步：请点击左下角')}“<IconFont type="icon-danchuangicon1" />”{t('选择')}“<IconFont type="icon-danchuangicon2" />”</div>, img: require(`../../assets/step_1${isZhCn ? '':'_en' }.png`) },
+	// 		{ title: <div>{t('第一步：请点击左下角')}“<IconFont type="icon-danchuangicon2" />”</div>, img: require(`../../assets/step_1_1${isZhCn ? '':'_en' }.jpg`) },
+	// 		{ title: <div>{t('第二步：进入钱包后请点击右上角')} <br />“<IconFont type="icon-danchuangicon3" />”{t('按钮')}</div>, img: require(`../../assets/step_2_2${isZhCn ? '':'_en' }.jpg`) },
+	// 		{ title: <div>{t('第三步：扫码绑定成功')}</div>, img: require(`../../assets/step_3${isZhCn ? '':'_en' }.png`) },
+	// 	],
+	// 	imToken: [
+	// 		{ title: t('第一步：进入钱包主页，点击右上角扫一扫'), img: require(`../../assets/imtoken_setp_1${isZhCn ? '' : '_en' }.png`) },
+	// 		{ title: <div>{t('第二步：扫码绑定成功')}</div>, img: require(`../../assets/step_3${isZhCn ? '' : '_en' }.png`) },
+	// 	],
+	// };
+
 	let carouselSetupTypeConfig:any = {
 		TokenPocket: [
-			{ title: t('第一步：进入钱包主页，点击右上角扫一扫'), img: require(`../../assets/tp_setp_1${isZhCn ?'' :'_en'}.jpg`) },
-			{ title: <div>{t('第二步：扫码绑定成功')}</div>, img: require(`../../assets/step_3${isZhCn ?'' :'_en'}.png`) },
+			{ title: <div>{t('设备端：通过【菜单】-【Dapp二维码】调出绑定所需二维码')} {t('手机端：进入钱包，找到扫一扫功能，扫描设备端显示的二维码即可绑定')}</div> },
 		],
 		MateMask: [
-			// { title: <div>{t('第一步：请点击左下角')}“<IconFont type="icon-danchuangicon1" />”{t('选择')}“<IconFont type="icon-danchuangicon2" />”</div>, img: require(`../../assets/step_1${isZhCn ? '':'_en' }.png`) },
-			{ title: <div>{t('第一步：请点击左下角')}“<IconFont type="icon-danchuangicon2" />”</div>, img: require(`../../assets/step_1_1${isZhCn ? '':'_en' }.jpg`) },
-			{ title: <div>{t('第二步：进入钱包后请点击右上角')} <br />“<IconFont type="icon-danchuangicon3" />”{t('按钮')}</div>, img: require(`../../assets/step_2_2${isZhCn ? '':'_en' }.jpg`) },
-			{ title: <div>{t('第三步：扫码绑定成功')}</div>, img: require(`../../assets/step_3${isZhCn ? '':'_en' }.png`) },
+			{ title: <div>{t('设备端：通过【菜单】-【Dapp二维码】调出绑定所需二维码')} {t('手机端：进入钱包，找到扫一扫功能，扫描设备端显示的二维码即可绑定')}</div> },
 		],
 		imToken: [
-			{ title: t('第一步：进入钱包主页，点击右上角扫一扫'), img: require(`../../assets/imtoken_setp_1${isZhCn ? '' : '_en' }.png`) },
-			{ title: <div>{t('第二步：扫码绑定成功')}</div>, img: require(`../../assets/step_3${isZhCn ? '' : '_en' }.png`) },
+			{ title: <div>{t('设备端：通过【菜单】-【Dapp二维码】调出绑定所需二维码')} {t('手机端：进入钱包，找到扫一扫功能，扫描设备端显示的二维码即可绑定')}</div> },
 		],
+		
 	};
 
 	let carouselType: any = 'TokenPocket';
@@ -75,12 +88,18 @@ export const BindDeviceCarousel = (props: IbindDeviceCarouselProps) => {
 
 	// console.log(carouselSetupType[carouselType], 'carouselSetupType[carouselType]', carouselType);
 
-	return <Carousel afterChange={afterChange} className="add_device_carousel" dotActiveStyle={{ backgroundColor: "#1677FF" }}>
-		{carouselSetupType[carouselType]?.map((item:any, key:any) => {
-			return <div key={key} className="setp_box">
-				<div className="setp_title">{item.title}</div>
-				<img style={{ width: '100%' }} src={(item.img)} alt="" />
+	// return <Carousel afterChange={afterChange} className="add_device_carousel" dotActiveStyle={{ backgroundColor: "#1677FF" }}>
+	// 	{carouselSetupType[carouselType]?.map((item:any, key:any) => {
+	// 		return <div key={key} className="setp_box">
+	// 			<div className="setp_title">{item.title}</div>
+	// 			<img style={{ width: '100%' }} src={(item.img)} alt="" />
+	// 		</div>
+	// 	})}
+
+	return <div className='add_device_carousel'>
+			<div className="setp_box">
+				<div className="setp_title">{t('设备端：通过【菜单】-【Dapp二维码】调出绑定所需二维码')}</div>
+				<div className="setp_title">{t('手机端：进入钱包，找到扫一扫功能，扫描设备端显示的二维码即可绑定')}</div>
 			</div>
-		})}
-	</Carousel>;
+		</div>
 }
